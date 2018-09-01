4ae86474b5afa7bf50d7f30cfba81221de74be67
Vishwas <gopinav5@gmail.com>
2018-09-01 18:48:38 +0530

    Typography

--- material-demo/src/app/app.module.ts
+++ material-demo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TypographyComponent } from './typography/typography.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypographyComponent
   ],
   imports: [
     BrowserModule,

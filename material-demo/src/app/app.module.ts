28c7340894435077e69367054196417ab2c82083
Vishwas <gopinav5@gmail.com>
2018-09-03 17:39:10 +0530

    Button initial Commit

--- material-demo/src/app/app.module.ts
+++ material-demo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TypographyComponent } from './typography/typography.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TypographyComponent
+    TypographyComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,

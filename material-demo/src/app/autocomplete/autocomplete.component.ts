db63be8b8ef9071a2fd9676e6f1d43db083e2cee
Vishwas <gopinav5@gmail.com>
2019-01-25 20:38:26 +0530

    Filter autocomplete

--- material-demo/src/app/autocomplete/autocomplete.component.ts
+++ material-demo/src/app/autocomplete/autocomplete.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormControl } from '@angular/forms'
+import { Observable } from 'rxjs'
+import { map, startWith } from 'rxjs/operators'
 
 @Component({
   selector: 'app-autocomplete',
@@ -6,13 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autocomplete.component.css']
 })
 export class AutocompleteComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    )
   }
 
-  // myControl = new FormControl();
-  options: string[] = ['Angular', 'React', 'Vue'];
+  myControl = new FormControl()
+  options: string[] = ['Angular', 'React', 'Vue']
+  objectOptions = [
+    { name: 'Angular' },
+    { name: 'Angular Material' },
+    { name: 'React' },
+    { name: 'Vue' }
+  ]
+  filteredOptions: Observable<string[]>
 
+  displayFn(subject) {
+    console.log(subject)
+    return subject ? subject.name : undefined
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase()
+    return this.options.filter(option =>
+      option.toLowerCase().includes(filterValue)
+    )
+  }
 }
